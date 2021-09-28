f3b859f2e45021c10eb5d3ad0bdb4e50685cb270
Jean Carlos A <jeanc.developer@outlook.com>
2021-09-28 12:31:39 -0300

    Add delete set and login Se agrega las funciones de eliminar,editar producto y la vista del login y registro

--- src/main/java/com/example/webappjava/controller/HomeController.java
+++ src/main/java/com/example/webappjava/controller/HomeController.java
@@ -12,4 +12,10 @@ public class HomeController {
     public String index() {
         return "index";
     }
+
+    @GetMapping(value = { "/login"})
+    public String login() {
+        return "login";
+    }
+
 }
--- src/main/java/com/example/webappjava/controller/ProductController.java
+++ src/main/java/com/example/webappjava/controller/ProductController.java
@@ -5,10 +5,7 @@ import com.example.webappjava.service.ProductService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -34,6 +31,7 @@ public class ProductController {
     }
 
 
+    // Create new Product
     @PostMapping("/creator")
     public ModelAndView creator(@RequestParam String name, @RequestParam float price){
         ModelAndView mv = new ModelAndView();
@@ -58,5 +56,73 @@ public class ProductController {
 //        Here put URL to want redirect
         mv.setViewName("redirect:/product/list");
         return mv;
+    }
+
+    @GetMapping("/details/{id}")
+    public ModelAndView details(@PathVariable("id") int id ) {
+        if (!productService.existsById(id))
+            return new ModelAndView("redirect:/product/list");
+        Product product = productService.getOne(id).get();
+        ModelAndView mv = new ModelAndView("/product/ProductDetails");
+        mv.addObject("product", product);
+        return mv;
+    }
+
+    @PostMapping("/update")
+    public ModelAndView update(@RequestParam int id,
+                               @RequestParam String name,
+                               @RequestParam float price) {
+        // Check if exists
+        if (!productService.existsById(id))
+            return new ModelAndView("redirect:/product/list");
+
+        ModelAndView mv = new ModelAndView();
+        Product product = productService.getOne(id).get();
+
+        if(StringUtils.isBlank(name)){
+            mv.setViewName("product/ProductNew");
+            mv.addObject("product", product);
+            mv.addObject("error", "el nombre no puede estar vacío");
+            return mv;
+        }
+        if(price <1 ){
+            mv.setViewName("product/ProductSet");
+            mv.addObject("product", product);
+            mv.addObject("error", "el precio debe ser mayor que cero");
+            return mv;
+        }
+        if(productService.existsByName(name) && productService.getByName(name).get().getId() != id){
+            mv.setViewName("product/ProductSet");
+            mv.addObject("product", product);
+            mv.addObject("error", "ese nombre ya existe");
+            return mv;
+        }
+        product.setName(name);
+        product.setPrice(price);
+        productService.save(product);
+        return new ModelAndView("redirect:/product/list");
+    }
+
+    // When clicked icon edit
+    @GetMapping("/edit/{id}")
+    public ModelAndView edit(@PathVariable("id") int id) {
+        // Check if exists
+        if (!productService.existsById(id))
+            return new ModelAndView("redirect:/product/list");
+
+        Product product = productService.getOne(id).get();
+        ModelAndView mv = new ModelAndView("/product/ProductSet");
+        mv.addObject("product", product);
+        return mv;
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView delete(@PathVariable("id") int id){
+        // Check if exists
+        if (productService.existsById(id)){
+            productService.delete(id);
+            return new ModelAndView("redirect:/product/list");
+        }
+    return null;
     }
 }
